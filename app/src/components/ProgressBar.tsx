7e56333ef2dffc584f4485c9eb3958bfa2014d7c
Xiao Zhong <xzhong.dev@gmail.com>
2022-03-17 20:51:52 -0600

    prettify progress bar and add percentage label

--- app/src/components/ProgressBar.tsx
+++ app/src/components/ProgressBar.tsx
@@ -1,4 +1,4 @@
-import { Box, LinearProgress } from "@mui/material";
+import { Box, LinearProgress, Typography } from "@mui/material";
 
 import { ProgressBarProps } from "../utils/types";
 
@@ -6,8 +6,23 @@ const ProgressBar = ({ fundingGoal, totalFundings }: ProgressBarProps) => {
   const progress: number = (totalFundings / fundingGoal) * 100;
 
   return (
-    <Box sx={{ width: "100%" }}>
-      <LinearProgress variant="determinate" value={progress} />
+    <Box sx={{ display: "flex", alignItems: "center", width: "100%" }}>
+      <Box sx={{ width: "100%", mr: 1, height: 20, display: "table" }}>
+        <LinearProgress
+          variant="determinate"
+          value={progress}
+          sx={{
+            borderRadius: 5,
+            display: "table-cell",
+            verticalAlign: "middle",
+          }}
+        />
+      </Box>
+      <Box sx={{ minWidth: 5 }}>
+        <Typography variant="body2" color="text.secondary">
+          {`${Math.round(progress)}%`}
+        </Typography>
+      </Box>
     </Box>
   );
 };
