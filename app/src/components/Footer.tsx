278898c80307e4cceae0de3d1e6e80da8b8453a4
Xiao Zhong <xzhong.dev@gmail.com>
2022-03-19 05:09:57 -0600

    update footer size

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -3,32 +3,38 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Footer = () => (
-  <ul
-    style={{
-      margin: "auto 0",
-      backgroundColor: "#00807b",
-      display: "flex",
-      justifyContent: "space-evenly",
-      alignItems: "center",
-      listStyle: "none",
-      gap: "30px",
-    }}
-  >
-    <li>
-      <a href="https://github.com/xiaozhong21" target="_blank" rel="noreferrer">
-        <GitHubIcon sx={{ color: "white" }} />
-      </a>
-    </li>
-    <li>
-      <a
-        href="https://www.linkedin.com/in/xiaozhong/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <LinkedInIcon sx={{ color: "white" }} />
-      </a>
-    </li>
-  </ul>
+  <div style={{ height: "30px", marginTop: "100px" }}>
+    <ul
+      style={{
+        margin: "auto 0",
+        backgroundColor: "#00807b",
+        display: "flex",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        listStyle: "none",
+        gap: "30px",
+      }}
+    >
+      <li>
+        <a
+          href="https://github.com/xiaozhong21"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <GitHubIcon sx={{ color: "white", height: 53, width: 36 }} />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.linkedin.com/in/xiaozhong/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <LinkedInIcon sx={{ color: "white", height: 53, width: 36 }} />
+        </a>
+      </li>
+    </ul>
+  </div>
 );
 
 export default Footer;
